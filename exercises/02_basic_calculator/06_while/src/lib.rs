1d55f10b087bf5691867c9d93162669005600046
renasami <drrr0502@gmail.com>
2025-02-23 01:26:15 +0900

    02 is done

--- exercises/02_basic_calculator/06_while/src/lib.rs
+++ exercises/02_basic_calculator/06_while/src/lib.rs
@@ -4,7 +4,11 @@ pub fn factorial(n: u32) -> u32 {
     // interprets as "I'll get back to this later", thus
     // suppressing type errors.
     // It panics at runtime.
-    todo!()
+    if n == 0 {
+        return 1;
+    } else {
+        n * factorial(n - 1)
+    }
 }
 
 #[cfg(test)]
